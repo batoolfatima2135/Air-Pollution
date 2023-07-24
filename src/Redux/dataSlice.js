91600e6cd23d90fb30b6b99eb3dde04da2da4947
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-25 00:44:46 +0500

    Remove comment

--- src/Redux/dataSlice.js
+++ src/Redux/dataSlice.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
