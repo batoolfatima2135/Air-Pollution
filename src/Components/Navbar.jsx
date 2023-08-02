ee8ccfbc45ba3ab327e147d4e1442fa615dc82b1
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-02 13:45:40 +0500

    Add Navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,28 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
+import settings from '../Assets/settings.png';
+import mic from '../Assets/mic.png';
+import back from '../Assets/back.png';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li><NavLink to="/innerpage">innerpage</NavLink></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-expand-sm mx-2 bg-dark">
+      {currentUrl.includes('/details') ? <NavLink to="/"><img src={back} alt="back" height="20px" /></NavLink> : null}
+      <h3 className="text-light m-0">Air pollution Data</h3>
+      <ul className="navbar-nav ms-auto d-flex flex-row">
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            <img src={mic} alt="mic" height="30px" />
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            <img src={settings} alt="setting" />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
